a282a741568c706e522608e78d3177a53a968927
DLellep <david-andero.lellep@khk.ee>
2021-06-06 02:14:52 +0300

    1.6 parent element crisis

--- src/index.js
+++ src/index.js
@@ -1,65 +1,23 @@
-import React from "react";
 import ReactDOM from "react-dom";
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  );
-};
-const Content = (props) => {
-  return (
-    <div>
-      <Part parts={props.p[0].name} exs={props.p[0].exs} />
-      <Part parts={props.p[1].name} exs={props.p[1].exs} />
-      <Part parts={props.p[2].name} exs={props.p[2].exs} />
-    </div>
-  );
-};
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-        Number of exercises = {props.p[0].exs + props.p[1].exs + props.p[2].exs}
-      </p>
-    </div>
-  );
-};
-const Part = (props) => {
-  return (
-    <p>
-      {props.parts} {props.exs} 
-    </p>
-  );
-};
-
+import React, { useState } from 'react'
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    p: [
-      {
-        name: "Fundamentals of React = ",
-        exs: 10,
-      },
-      {
-        name: "Using props to pass data = ",
-        exs: 7,
-      },
-      {
-        name: "State of a component = ",
-        exs: 14,
-      }
-    ]
-  }
+
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    //if these values are still undefined im giving up
-    <div>
-      <Header course={course.name} />
-      <Content p={course.p} />
-      <Total p={course.p} />
-
-    </div>
+    <div class="ispentwaytoomuchtimefiguringoutwhythisisnotworkinganditturnsoutijustneedaparentelement">
+      <h1>feedbck</h1>
+      <button onClick={()=>setGood(good+1)}>good</button>
+      <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
+      <button onClick={()=>setBad(bad+1)}>bad</button>
+      <h1>statisitics</h1>
+      <p>Good = {good}</p>
+      <p>Neutral = {neutral}</p>
+      <p>Bad =  {bad}</p></div>
   );
 };
+
 ReactDOM.render(<App />, document.getElementById("root"));
