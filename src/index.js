7ac809964f2700ed2884f65f53858035b27043c3
DLellep <david-andero.lellep@khk.ee>
2021-06-06 02:09:58 +0300

    1.5

--- src/index.js
+++ src/index.js
@@ -16,49 +16,49 @@ const Content = (props) => {
     </div>
   );
 };
-
-
 const Total = (props) => {
   return (
     <div>
       <p>
-
-        {props.p[0].exs + props.p[1].exs + props.p[2].exs}
+        Number of exercises = {props.p[0].exs + props.p[1].exs + props.p[2].exs}
       </p>
     </div>
   );
 };
-
 const Part = (props) => {
   return (
     <p>
-      {props.parts} {props.exs}
+      {props.parts} {props.exs} 
     </p>
   );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const p = [
-    {
-      name: "Fundamentals of React = ",
-      exs: 10,
-    },
-    {
-      name: "Using props to pass data = ",
-      exs: 7,
-    },
-    {
-      name: "State of a component = ",
-      exs: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    p: [
+      {
+        name: "Fundamentals of React = ",
+        exs: 10,
+      },
+      {
+        name: "Using props to pass data = ",
+        exs: 7,
+      },
+      {
+        name: "State of a component = ",
+        exs: 14,
+      }
+    ]
+  }
 
   return (
+    //if these values are still undefined im giving up
     <div>
-      <Header course={course} />
-      <Content p={p} />
-      <Total p={p} />
+      <Header course={course.name} />
+      <Content p={course.p} />
+      <Total p={course.p} />
+
     </div>
   );
 };
